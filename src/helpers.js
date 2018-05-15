d02da3e9ffa16d24d9ace60f47c7969b8d7d507b
Boris Shevchenko <bshevchenkoweb@gmail.com>
2018-05-15 22:06:28 +0300

    thousandsDelimiter fix for fraction

--- src/helpers.js
+++ src/helpers.js
@@ -35,8 +35,10 @@ export const etherscanToken = (address: string, label?: string | Node) => {
   return etherscan('token', address, label || address)
 }
 
-export const thousandsDelimiter = (v: number) =>
-  v.toString(10).replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+export const thousandsDelimiter = (v: number) => {
+  let [i, f] = v.toString(10).split('.')
+  return i.replace(/\B(?=(\d{3})+(?!\d))/g, ',') + (f ? ('.' + f) : '')
+}
 
 export const addressShortifier = (address: string) =>
   address.substring(0, 7) + '...' + address.slice(-7)
